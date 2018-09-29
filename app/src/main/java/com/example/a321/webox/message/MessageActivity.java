444f4c9c56d670bb5064475b7711fec2fcd6c8b9
321 <1748812238@qq.com>
2018-09-29 17:51:28 +0800

    添加消息页listview的item布局文件

--- app/src/main/java/com/example/a321/webox/message/MessageActivity.java
+++ app/src/main/java/com/example/a321/webox/message/MessageActivity.java
@@ -4,17 +4,34 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.a321.webox.R;
 
 public class MessageActivity extends Fragment {
 
+    private TextView tv;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_message,container,false);
+        View view = inflater.inflate(R.layout.activity_message,container,false);
+        tv = view.findViewById(R.id.tv_activity_message);
+        return view;
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if(hidden){
+
+        }else {
+
+        }
     }
 }
