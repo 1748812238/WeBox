6f57daa9e81aea400068fde5349ce7a32585f802
321 <1748812238@qq.com>
2018-09-29 16:18:21 +0800

    WeBox添加底部导航栏

--- app/src/main/java/com/example/a321/webox/MainActivity.java
+++ app/src/main/java/com/example/a321/webox/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.a321.webox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.a321.webox.contacts.ContactsActivity;
+import com.example.a321.webox.mylist.MyListActivity;
+import com.example.a321.webox.message.MessageActivity;
+import com.example.a321.webox.sendcircle.SendCircleActivity;
+import com.hjm.bottomtabbar.BottomTabBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomTabBar bottomTabBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
     }
+
+    private void initView(){
+        bottomTabBar = findViewById(R.id.bottom_bar);
+        bottomTabBar.init(getSupportFragmentManager())
+                .addTabItem("消息",R.drawable.msga,R.drawable.msg,MessageActivity.class)
+                .addTabItem("联系人",R.drawable.contacta,R.drawable.contact,ContactsActivity.class)
+                .addTabItem("发圈",R.drawable.snsa,R.drawable.sns,SendCircleActivity.class)
+                .addTabItem("列表",R.drawable.mya,R.drawable.my,MyListActivity.class)
+                .setOnTabChangeListener(new BottomTabBar.OnTabChangeListener() {
+                    @Override
+                    public void onTabChange(int position, String name, View view) {
+
+                    }
+                })
+                .setCurrentTab(0);
+    }
+
 }
---
+++ app/src/main/java/com/example/a321/webox/sendcircle/SendCircleActivity.java
@@ -0,0 +1,20 @@
+package com.example.a321.webox.sendcircle;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a321.webox.R;
+
+public class SendCircleActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_sendcricle,container,false);
+    }
+}
