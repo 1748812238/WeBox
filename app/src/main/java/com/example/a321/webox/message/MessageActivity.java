ece2d01d9f5067841e12ffa061bf395744c36ec5
321 <1748812238@qq.com>
2018-09-29 18:09:22 +0800

    添加消息页listview视图

--- app/src/main/java/com/example/a321/webox/message/MessageActivity.java
+++ app/src/main/java/com/example/a321/webox/message/MessageActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,13 +16,13 @@ import com.example.a321.webox.R;
 
 public class MessageActivity extends Fragment {
 
-    private TextView tv;
+    private ListView lv;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_message,container,false);
-        tv = view.findViewById(R.id.tv_activity_message);
+        lv = view.findViewById(R.id.lv_activity_message);
         return view;
     }
 
