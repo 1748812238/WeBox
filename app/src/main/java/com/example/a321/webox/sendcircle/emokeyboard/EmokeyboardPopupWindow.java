48f34a59fbbcce5e8bbfc440f480832a878ca630
321 <1748812238@qq.com>
2018-10-05 10:32:49 +0800

    删除emokeyboard

--- app/src/main/java/com/example/a321/webox/sendcircle/emokeyboard/EmokeyboardPopupWindow.java
+++
@@ -1,9 +0,0 @@
-package com.example.a321.webox.sendcircle.emokeyboard;
-
-import android.widget.PopupWindow;
-
-public class EmokeyboardPopupWindow extends PopupWindow {
-
-
-
-}
--- app/src/main/java/com/example/a321/webox/sendcircle/emokeyboard/util/EmotionUtils.java
+++
@@ -1,4 +0,0 @@
-package com.example.a321.webox.sendcircle.emokeyboard.util;
-
-public class EmotionUtils {
-}
